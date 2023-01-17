eefd29a9f224b7e4b059fc1727d154993ca18a12
wojciech mierzejewski <mierzvoj@gmail.com>
2023-01-17 23:13:56 +0100

    8th

--- src/test/java/webdemo/seleniumDemo/TestXPath.java
+++ src/test/java/webdemo/seleniumDemo/TestXPath.java
@@ -91,7 +91,7 @@ public class TestXPath {
 
     @Test
     public void testExactAtributeNode() {
-        WebElement element = driver.findElement(By.xpath("//title[@value = 'Google']"));
+        WebElement element = driver.findElement(By.xpath("//id[@value = 'input']"));
         System.out.println(element.getAttribute("value"));
         assertNotNull(element);
     }
