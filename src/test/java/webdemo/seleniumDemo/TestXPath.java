517577a8a1705cdeb2bc7527a7b7528e20dc31f7
wojciech mierzejewski <mierzvoj@gmail.com>
2023-01-17 22:43:00 +0100

    5th

--- src/test/java/webdemo/seleniumDemo/TestXPath.java
+++ src/test/java/webdemo/seleniumDemo/TestXPath.java
@@ -91,7 +91,7 @@ public class TestXPath {
 
     @Test
     public void testExactAtributeNode() {
-        WebElement element = driver.findElement(By.xpath("//input[@value = 'Szukaj w Google']"));
+        WebElement element = driver.findElement(By.xpath("//input[@value = 'Search Google:']"));
         System.out.println(element.getAttribute("value"));
         assertNotNull(element);
     }
